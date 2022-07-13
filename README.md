67accc17f3d27dd7cdeaca8898c58aea5344b0b1
Niceygy <niceygy@niceygylive.xyz>
2022-07-13 17:19:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# counter66
+A Discord bot that adds in a counting channel.
+
+Project ran by @Parv66
---
+++ server.js
@@ -0,0 +1,24 @@
+let Discord = require("discord.js")
+//const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({ intents: [ 'DIRECT_MESSAGES', 'GUILD_MESSAGES' ] });
+const { prefix, botToken } = require('./config.json')
+
+console.log("Stage:1 Sucsessful!")
+
+client.login(botToken);
+console.log("Stage 2: Sucsessfull!")
+
+client.on("ready", async () => {
+  console.log("Running!")
+});
+
+//client.on("message", async message => {
+//    message.channel.send('Pong!')
+//  })
+
+
+//command(client, ['ping'], (message) => {
+//   message.channel.send('Pong!')
+// })
+
+
