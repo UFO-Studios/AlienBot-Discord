78ad33bc26b8034663df9e499f5dd91beacf51f9
Niceygy <nceygy01@gmail.com>
2022-07-15 20:17:27 +0100

    fixed bug obvs

--- commands/woof.js
+++ commands/woof.js
@@ -6,8 +6,8 @@ module.exports = {
 		.setDescription('The bot goes woof.'),
 	async execute(interaction) {
 		await interaction.reply('WOOF. WOOF WOOF WOOF. WOOFWOOFWOOFBARK');
-        await interaction.reply({ content: 'Nell is the best!', ephemeral: true });
-        console.log("woof");
+        //await interaction.reply({ content: 'Nell is the best!', ephemeral: true });
+        //console.log("woof");
 	},
 };
 console.log("woof online")
