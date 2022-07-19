d63479b7b069d6a6b5fa3e38a9afba14f1fc35f5
Niceygy <nceygy01@gmail.com>
2022-07-19 18:44:38 +0100

    fixed ufosmp

--- commands/ufosmp.js
+++ commands/ufosmp.js
@@ -1,23 +1,22 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName("UFO SMP")
-//     .setDescription("Info about the UFO SMP"),
-//   async execute(interaction) {
-//     await interaction.reply(
-//       "UFO SMP is a server run by alien for his discord! Want to join us? But your on bedrock? FEAR NOT! It runs GeyserMC for crossplay. Still want to join? Get to level 3 (`!rank`) and apply through #create-a-ticket :D"
-//     );
-//   },
-// };
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('woofwoof')
-		.setDescription('The bot goes woof.'),
-	async execute(interaction) {
-		await interaction.reply('WOOF. WOOF WOOF WOOF. WOOFWOOFWOOFBARK');
-	},
+ module.exports = {
+   data: new SlashCommandBuilder()
+     .setName("ufosmp")
+    .setDescription("Info about the UFO SMP"),
+   async execute(interaction) {
+     await interaction.reply("UFO SMP is a server run by alien for his discord! Want to join us? But your on bedrock? FEAR NOT! It runs GeyserMC for crossplay. Still want to join? Get to level 3 (`!rank`) and apply through #create-a-ticket :D"
+     );
+   },
 };
+
+//module.exports = {
+//	data: new SlashCommandBuilder()
+//		.setName('woofwoof')
+//		.setDescription('The bot goes woof.'),
+//	async execute(interaction) {
+//		await interaction.reply('WOOF. WOOF WOOF WOOF. WOOFWOOFWOOFBARK');
+//	},
+//};
 
 console.log("ufosmp.js run");
