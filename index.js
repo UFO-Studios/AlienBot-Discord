20572f2c6d4b82b8347fd49e2ff0d8cda1853293
Niceygy <nceygy01@gmail.com>
2022-07-19 19:44:22 +0100

    clearing up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+  .filter((file) => file.endsWith(".js")); 
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
