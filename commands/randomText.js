a28ec56cb6ef4d89fda594eeffb94136b22bfc0d
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-20 23:34:46 +0530

    "changes": fixed randomText.js, formatted mariadb

--- commands/randomText.js
+++ commands/randomText.js
@@ -1,49 +1,22 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { sentence, paragraph, article } = require("txtgen/dist/cjs/txtgen.js");
-const { splitMessage } = require("discord.js/Utils");
-
-const getTextArray = async (Text) => {
-  if (Text.length > 2000) {
-    return Text.split("/(?<=^(?:.{2000})+)(?!$)/g");
-  }
-};
 
 const Lucky = (randomNum) => {
   if (randomNum == 69) {
     const randomN = Math.floor(Math.random() * 4);
-    switch (randomN) {
-      case 1:
-        return "https://thealiendoctor.com/ is cool";
-      case 2:
-        return "Sub 2 Alien";
-      case 3:
-        return "join the discord! https://discord.gg/dKaptM4Pgm";
-      case 4:
-        return "cool beanz™️";
+    if (randomN == 1) {
+      return "https://thealiendoctor.com is cool"
+    } else if (randomN == 2) {
+      return "Sub 2 Alien"
+    } else if (randomN == 3) {
+      return "Join the discord! https://discord.gg/dKaptM4Pgm"
+    } else if (randomN == 4) {
+      return "cool beanz™️"
     }
   } else {
     return "";
   }
 };
-
-// const articles = async (Text, interaction) => {
-//   const randomNum = Math.floor(Math.random() * 100);
-//   if (Text.length > 2000) {
-//     const array = await Text.match("/.{1,3}/g");
-//     console.log(array);
-//     await interaction.channel.send(
-//       `Heres the generated text: \n ${Lucky(randomNum)} \n`
-//     );
-//     await array.map((text) => {
-//       interaction.channel.send(text);
-//     });
-//     return;
-//   } else {
-//     interaction.reply(
-//       `Heres the generated text: \n ${Lucky(randomNum)} \n ${Text}`
-//     );
-//   }
-// };
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -55,8 +28,7 @@ module.exports = {
         .setDescription("Text to generate")
         .addChoices(
           { name: "sentence", value: "sentence" },
-          { name: "paragraph", value: "paragraph" },
-          { name: "article", value: "article" }
+          { name: "paragraph", value: "paragraph" }
         )
         .setRequired(true);
       return option;
@@ -75,7 +47,7 @@ module.exports = {
       const randomNum = Math.floor(Math.random() * 100);
       const random = Lucky(randomNum);
 
-      return interaction.channel.send(
+      return interaction.reply(
         `Heres the generated text: \n  ${random} \n ${Text}`,
         { split: true }
       );
