fa333e128f4614b3dc3e0f0761cd7e4d96eb35d9
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-22 23:34:05 +0530

    *well*

--- firebaseTest.js
+++
@@ -1,7 +0,0 @@
-const firebase = require("./firebase");
-
-(async () => {
-  const lmao = await firebase.getData("lmao", "lmao");
-  console.log(lmao);
-  firebase.addData("lmao", "lmao2", { lmao: lmao.lmao + 1 });
-})();
