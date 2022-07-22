7a2d4404def9104708ef143af66ac933f56769c1
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-22 23:46:03 +0530

    updated avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,14 +1,31 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('avatar')
-		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
-		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
-	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-		return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
-	},
+  data: new SlashCommandBuilder()
+    .setName("pfp")
+    .setDescription("Get the profile picture of a member")
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The target to show the pfp of")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    const user = await interaction.options.getUser("target");
+
+    const embed = new MessageEmbed()
+      .setAuthor(interaction.user.tag)
+      .setColor("RANDOM")
+      .setTitle("Profile Picture")
+      .setDescription("Heres the profile picture of the targeted user:")
+      .setImage(user.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setFooter({ text: "/pfp • AlienBot" });
+
+    return await interaction.editReply({ embeds: [embed] });
+  },
 };
-console.log("avatar.js run")
+console.log("avatar.js run");
