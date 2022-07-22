fd4839df57f0af5a2db65ae10f864e4f5d116c18
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-22 20:59:37 +0530

    uhhhh lets not get banned

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const BW = require("./b-w.json");
+const Config = require("./config.json")
 const db = require("easy-db-json");
 const { Emoji } = require("discord.js");
 
@@ -13,7 +13,7 @@ module.exports = {
       const array = await message.content.split(" ");
 
       array.map((word) => {
-        if (BW["BANNED-WORDS"].includes(word)) {
+        if (Config["BANNED-WORDS"].includes(word)) {
           message.reply({ content: "You cannot use that word!", reply: true });
           setTimeout(() => {
             message.delete();
