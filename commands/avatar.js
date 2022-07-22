45b83c91c56bc5af94076d8e9fbfcbf6657589b8
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-23 00:00:02 +0530

    removed deferReply() wasnt needed

--- commands/avatar.js
+++ commands/avatar.js
@@ -12,20 +12,18 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    await interaction.deferReply();
-
     const user = await interaction.options.getUser("target");
 
     const embed = new MessageEmbed()
       .setAuthor(interaction.user.tag)
       .setColor("RANDOM")
       .setTitle("Profile Picture")
-      .setDescription("Heres the profile picture of the targeted user:")
+      .setDescription(`Heres the profile picture of the \`${user.tag}\`:`)
       .setImage(user.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setFooter({ text: "/pfp • AlienBot" });
 
-    return await interaction.editReply({ embeds: [embed] });
+    return await interaction.reply({ embeds: [embed] });
   },
 };
 console.log("avatar.js run");
