605e7883d96d88e966b8c4bb24468de040345a06
Niceygy <nceygy01@gmail.com>
2022-07-22 11:11:27 +0100

    random bread func

--- commands/bread.js
+++ commands/bread.js
@@ -1,12 +1,20 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+// good or bad...?!?
+const random = Math.random()
+
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("bread")
-    .setDescription("bread :thumbsup:"),
+    .setDescription("bread...?!?"),
   async execute(interaction, client) {
+    if (random > 0.5){
     return interaction.reply("Bread :thumbsup:");
-  },
+    } else {
+      return interaction.reply("Bread :thumbsdown:");
+      }
+    },
 };
 
 console.log("Bread.js run")
