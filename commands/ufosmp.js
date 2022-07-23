a459117d5cb4e41fbe2227fb60391bcc2794a9cc
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-23 18:33:12 +0530

    change: user-info cmd and ufo-smp cmd improved

--- commands/ufosmp.js
+++ commands/ufosmp.js
@@ -1,22 +1,27 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
 
- module.exports = {
-   data: new SlashCommandBuilder()
-     .setName("ufosmp")
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ufosmp")
     .setDescription("Info about the UFO SMP"),
-   async execute(interaction) {
-     await interaction.reply("UFO SMP is a server ru by Alien for members of The Alien Empire community! Want to join us? But you're  on bedrock? FEAR NOT! It runs GeyserMC for crossplay. Still want to join? Get to level 5 (`/rank`) and apply on the form in the info channel! :D"
-     );
-   },
+  async execute(interaction) {
+    const embed = new MessageEmbed()
+      .setAuthor(interaction.user.tag)
+      .setColor("RANDOM")
+      .setTitle("UFO SMP")
+      .setDescription(
+        "UFO SMP is a server run by Alien for members of The Alien Empire community! Want to join us? But you're  on bedrock? FEAR NOT! It runs GeyserMC for crossplay. Still want to join? Get to level 5 (`/rank`) and apply on the form in the info channel! :D"
+      )
+      .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setFooter({
+        text: "/ufosmp • Alienbot",
+        iconURL:
+          "https://cdn.discordapp.com/app-icons/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.png?size=64&quot",
+      });
+    return await interaction.reply({ embeds: [embed] });
+  },
 };
-
-//module.exports = {
-//	data: new SlashCommandBuilder()
-//		.setName('woofwoof')
-//		.setDescription('The bot goes woof.'),
-//	async execute(interaction) {
-//		await interaction.reply('WOOF. WOOF WOOF WOOF. WOOFWOOFWOOFBARK');
-//	},
-//};
 
 console.log("ufosmp.js run");
--- commands/user-info.js
+++ commands/user-info.js
@@ -1,11 +1,105 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Permissions } = require("discord.js");
+
+const getPerms = async (perms) => {
+  if (perms.has(Permissions.FLAGS.ADMINISTRATOR)) {
+    return "Administrator";
+  } else if (
+    perms.has([
+      Permissions.FLAGS.BAN_MEMBERS,
+      Permissions.FLAGS.MODERATE_MEMBERS,
+      Permissions.FLAGS.KICK_MEMBERS,
+    ])
+  ) {
+    return "Moderator";
+  } else if (
+    perms.has([
+      Permissions.FLAGS.MANAGE_CHANNELS,
+      Permissions.FLAGS.MANAGE_GUILD,
+      Permissions.FLAGS.MANAGE_MESSAGES,
+      Permissions.FLAGS.MANAGE_ROLES,
+      Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS,
+      Permissions.FLAGS.MANAGE_EVENTS,
+      Permissions.FLAGS.MANAGE_NICKNAMES,
+      Permissions.FLAGS.MANAGE_THREADS,
+      Permissions.FLAGS.MANAGE_WEBHOOKS,
+    ])
+  ) {
+    return "Server Manager";
+  } else {
+    return "member";
+  }
+};
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('user-info')
-		.setDescription('Display info about yourself.'),
-	async execute(interaction) {
-		return interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
-	},
+  data: new SlashCommandBuilder()
+    .setName("user-info")
+    .setDescription("Display info about a member.")
+    .addUserOption((option) =>
+      option.setName("target").setDescription("the target to get the info of")
+    ),
+  async execute(interaction) {
+    const targetMember =
+      interaction.options.getMember("target") || interaction.member;
+    const targetUser =
+      interaction.options.getUser("target") || interaction.user;
+
+    const roles = targetMember.roles.cache
+      .filter((role) => role.id !== interaction.guild.id)
+      .map((role) => role.name)
+      .join(", ");
+
+    const permsText = await getPerms(targetMember.permissions);
+
+    const embed = new MessageEmbed()
+      .setAuthor(targetUser.tag)
+      .setColor("RANDOM")
+      .setTitle("User Info")
+      .setThumbnail(targetUser.displayAvatarURL({ dynamic: true }))
+      .addFields(
+        {
+          name: "username",
+          value: `\`${targetUser.tag}\``,
+          inline: true,
+        },
+        {
+          name: "user id",
+          value: `\`${targetUser.id}\``,
+          inline: true,
+        },
+        {
+          name: "nickname",
+          value: `\`${targetMember.nickname || "no nickname"}\``,
+          inline: true,
+        },
+        {
+          name: "bot?",
+          value: `\`${targetUser.bot.toString()}\``,
+          inline: true,
+        },
+        {
+          name: "joined at",
+          value: `\`${targetMember.joinedAt}\``,
+          inline: true,
+        },
+        {
+          name: "created at",
+          value: `\`${targetUser.createdAt}\``,
+          inline: true,
+        },
+        {
+          name: "boosting since",
+          value: `\`${targetMember.premiumSince || "not boosting"}\``,
+          inline: true,
+        },
+        {
+          name: "permissions",
+          value: `\`${permsText}\``,
+          inline: true,
+        },
+        { name: "roles", value: `\`${roles || "No roles"}\`` }
+      );
+    return await interaction.reply({ embeds: [embed] });
+  },
 };
-console.log("user-info.js run")
+console.log("user-info.js run");
