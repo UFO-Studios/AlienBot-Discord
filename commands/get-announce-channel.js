0e75849ffe2ed5a944340a9a159f6061b6357cd0
kingerious <mohammedsiddiqui2009@gmail.com>
2022-07-30 16:22:57 +0530

    @niceygy i made some improvements

--- commands/get-announce-channel.js
+++ commands/get-announce-channel.js
@@ -3,30 +3,38 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("update-channel")
-    .setDescription("Sets the channel to publish the bot update/fixes messages.")
+    .setDescription(
+      "Sets the channel to publish the bot update/fixes messages."
+    )
     .addChannelOption((option) => {
       option
         .setName("channel")
-        .setDescription(
-          "The channel you want updates posted to."
-        )
+        .setDescription("The channel you want updates posted to.")
         .setRequired(true);
       return option;
     }),
   global: true,
 
-  async execute(interaction, client) { 
-    const serverId = await interaction.guildId 
-    const channel = await interaction.options.getChannel("channel")
-    console.log(channel) 
-    const channelId = channel.id 
-    client.F.getData("Upadate-channels", "varvaal", "varval") //determines how many entries we have
-    const varval = "varval"
-    const varvalnew = varval + 1
-    client.F.addData("Upadate-channels", varvalnew , { channelId, serverId })
-    await interaction.reply(`server id set to ${serverId}, & channelId set to ${channelId}. When there are any new updates/announcements about AlienBot you will get notified!`);
+  async execute(interaction, client) {
+    const channel = await interaction.options.getChannel("channel");
+
+    channel
+      .createWebhook({
+        name: "AlienBot",
+        avatar:
+          "https://cdn.discordapp.com/avatars/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.webps",
+      })
+      .then(async (webhook) => {
+        await client.F.addData("Update-channels", serverId, {
+          webhookUrl: webhook.url,
+        });
+
+        await interaction.reply(
+          `When there are any new updates/announcements about AlienBot you will get notified in ${channel}!`
+        );
+      });
   },
 };
-console.log("get-announce-channel.js run"); 
+console.log("get-announce-channel.js run");
 
 //client.F.addData("collectionName", "docName", { js: "object" })
