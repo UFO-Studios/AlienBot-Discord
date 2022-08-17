6bf9e505db42b42c9b152168583d8cba36a2f847
kingerious <mohammedsiddiqui2009@gmail.com>
2022-08-17 20:55:54 +0530

    some new commands

--- contextMenu/translateSpanish.js
+++ contextMenu/translateSpanish.js
@@ -9,7 +9,7 @@ const translate = require("@vitalets/google-translate-api");
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
-    .setName("translate-to-french")
+    .setName("translate-to-spanish")
     .setType(ApplicationCommandType.Message),
   /**
    *
@@ -19,26 +19,26 @@ module.exports = {
   async execute(interaction, client) {
     const msg = interaction.targetMessage.content;
 
-    const inFrench = await translate(msg, { to: "french" })
+    const inSpanish = await translate(msg, { to: "spanish" })
       .then((res) => res.text)
       .catch((e) => console.log(e));
 
     const embed = new EmbedBuilder()
       .setTitle("Translate")
-      .setDescription("Translate text to French.")
+      .setDescription("Translate text to Spanish.")
       .addFields(
         {
           name: "Original Text",
           value: `\`\`\`${msg}\`\`\``,
         },
-        { name: "Translated to French", value: `\`\`\`${inFrench}\`\`\`` }
+        { name: "Translated to Spanish", value: `\`\`\`${inSpanish}\`\`\`` }
       )
       .setColor("Blue")
       .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
       .setAuthor({ name: interaction.user.tag })
       .setTimestamp()
       .setFooter({
-        text: "Translate to French • Alienbot",
+        text: "Translate to Spanish • Alienbot",
         iconURL:
           "https://cdn.discordapp.com/app-icons/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.png?size=64&quot",
       });
@@ -47,4 +47,4 @@ module.exports = {
   },
 };
 
-console.log("translateFrench.js run");
+console.log("translateSpanish.js run");
