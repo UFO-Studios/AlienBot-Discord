7cf3e86802a7e6c040309797363e384ae11c6273
kingerious <mohammedsiddiqui2009@gmail.com>
2022-08-18 16:17:55 +0530

    /trashpost cmd

---
+++ commands/trashPost.js
@@ -0,0 +1,38 @@
+const {
+  SlashCommandBuilder,
+  Client,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+} = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("trashpost")
+    .setDescription("Get a random shitpost."),
+  global: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const data = await fetch("https://api.thedailyshitpost.net/random").then(
+      (r) => r.json()
+    );
+
+    // console.log(data);
+    if (data.error != "False") {
+      console.error(data.error);
+      return await interaction.reply({
+        content: "There was an error fetching a post!",
+      });
+    }
+
+    return await interaction.reply({
+      content: `Heres the trashpost: ${data.url}`,
+    });
+  },
+};
+
+console.log("trashPost.js run");
