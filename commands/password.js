5209b673dd663b42d4d8ab90ef9bd8c513eff96a
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2022-09-02 18:55:23 +0530

    random password cmd

---
+++ commands/password.js
@@ -0,0 +1,31 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  Client,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+const { default: fetch } = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("password")
+    .setDescription("Generates a random password for you."),
+  global: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const data = await fetch("https://poopoo-api.vercel.app/api/password").then(
+      (d) => d.json()
+    );
+
+    return await interaction.reply({
+      content: `Your custom generated password is: \`${data.password}\`. Keep it a secret, keep it safe.`,
+      ephemeral: true,
+    });
+  },
+};
+
+console.log("password.js run");
