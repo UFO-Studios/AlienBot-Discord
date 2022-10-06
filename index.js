15d05e912168e649cc442330cbeb50f6b2c32ae9
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2022-10-06 19:13:57 +0530

    fixed random text generator and added article option to generate text with more than 4000 words!

--- index.js
+++ index.js
@@ -44,23 +44,23 @@ const client = new Client({
   client.BANNED_WORDS = await data.WORDS;
 })();
 
-const player = new Player(client);
+const player = new Player(client, { ytdlOptions: { quality: 'highestaudio' } });
 
 player.on("trackStart", (queue, track) => {
-  const embed = new EmbedBuilder()
-    .setTitle("Play song")
-    .setDescription(`Now playing **${track.title}**!`)
-    .setColor("Green")
-    .setAuthor({ name: client.user.tag })
-    .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-    .setTimestamp()
-    .setFooter({
-      text: "Music System • Alienbot",
-      iconURL:
-        "https://cdn.discordapp.com/app-icons/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.png?size=64&quot",
-    });
+    const embed = new EmbedBuilder()
+        .setTitle("Play song")
+        .setDescription(`Now playing **${track.title}**!`)
+        .setColor("Green")
+        .setAuthor({ name: client.user.tag })
+        .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setFooter({
+            text: "Music System • Alienbot",
+            iconURL:
+            "https://cdn.discordapp.com/app-icons/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.png?size=64&quot",
+        });
 
-  queue.metadata.channel.send({ embeds: [embed] });
+    queue.metadata.channel.send({ embeds: [embed] });
 });
 
 client.discordTogether = new DiscordTogether(client);
@@ -72,24 +72,24 @@ client.Timeout = new Collection()
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
 }
 
 const contextPath = path.join(__dirname, "contextMenu");
 const contextFiles = fs
-  .readdirSync(contextPath)
-  .filter((f) => f.endsWith(".js"));
+    .readdirSync(contextPath)
+    .filter((f) => f.endsWith(".js"));
 
 for (const file of contextFiles) {
-  const filePath = path.join(contextPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
+    const filePath = path.join(contextPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
 }
 
 // const functionsPath = path.join(__dirname, "functions");
@@ -107,43 +107,42 @@ for (const file of contextFiles) {
 
 const imagesPath = path.join(__dirname, "./images/welcomeImages");
 const imagesFiles = fs
-  .readdirSync(imagesPath)
-  .filter((f) => f.endsWith(".png"));
+    .readdirSync(imagesPath)
+    .filter((f) => f.endsWith(".png"));
 client.images = [];
 
 for (const file of imagesFiles) {
-  client.images.push(path.join(imagesPath, file));
+    client.images.push(path.join(imagesPath, file));
 }
 
 const eventPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventPath).filter((f) => f.endsWith(".js"));
 
 for (const file of eventFiles) {
-  const filePath = path.join(eventPath, file);
-  const event = require(filePath);
+    const filePath = path.join(eventPath, file);
+    const event = require(filePath);
 
-  if (event.once) {
-    client.once(event.name, () => {
-      event.execute(client);
-    });
-  } else {
-    client.on(event.name, (...args) => {
-      event.execute(...args, client);
-    });
-  }
+    if (event.once) {
+        client.once(event.name, () => {
+            event.execute(client);
+        });
+    } else {
+        client.on(event.name, (...args) => {
+            event.execute(...args, client);
+        });
+    }
 }
 
 client.modals = new Collection();
 const modalsPath = path.join(__dirname, "modals");
 const modalFiles = fs
-  .readdirSync(modalsPath)
-  .filter((file) => file.endsWith(".js"));
+    .readdirSync(modalsPath)
+    .filter((file) => file.endsWith(".js"));
 
 for (const file of modalFiles) {
-  const filePath = path.join(modalsPath, file);
-  const modal = require(filePath);
-  client.modals.set(modal.name, modal);
+    const filePath = path.join(modalsPath, file);
+    const modal = require(filePath);
+    client.modals.set(modal.name, modal);
 }
 
 client.login(client.C.TOKEN);
-
