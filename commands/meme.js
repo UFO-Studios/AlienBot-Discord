536c14ff11cd1737fc99b6406be5864e88c4e370
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2022-12-02 18:58:51 +0530

    fixed meme command

--- commands/meme.js
+++ commands/meme.js
@@ -15,8 +15,6 @@ module.exports = {
    * @param {Client} client
    */
   async execute(interaction, client) {
-    await interaction.deferReply();
-
     const data = await fetch(
       `https://api.humorapi.com/memes/random?api-key=${client.C.MEME_API_KEY}`
     ).then((res) => res.json());
@@ -34,7 +32,7 @@ module.exports = {
           "https://cdn.discordapp.com/app-icons/800089810525356072/b8b1bd81f906b2c309227c1f72ba8264.png?size=64&quot",
       });
 
-    return await interaction.editReply({ embeds: [embed] });
+    return await interaction.reply({ embeds: [embed] });
   },
 };
 
