cb2dd5ae43cabbbf6d326670fc99f32a3779ab8f
NiceygyLive <niceygy@niceygylive.xyz>
2024-07-22 10:02:25 +0100

    cleanup

--- docker-push.sh
+++
@@ -1,6 +0,0 @@
-docker build \
- --label "org.opencontainers.image.source=https://github.com/ufostudios/alienbot-discord" / 
- --label "org.opencontainers.image.description=The Arm64 build of Alienbot" \
- --label "org.opencontainers.image.licenses=MIT" /
- -t ufostudios/alienbot-discord:arm64
-
